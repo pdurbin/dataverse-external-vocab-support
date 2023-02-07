d0c0c7f742c179ff5ec3719610459b34db3a36ae
Philip Durbin <philip_durbin@harvard.edu>
2023-02-07 14:32:28 -0500

    revert old config, split off new funders config

--- addsetting.sh
+++ addsetting.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -s http://localhost:8080/api/admin/settings/:CVocConf -X PUT --upload-file examples/config/cvoc-conf.json 
+curl -s http://localhost:8080/api/admin/settings/:CVocConf -X PUT --upload-file examples/config/funders.json 
